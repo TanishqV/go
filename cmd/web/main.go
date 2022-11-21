2e3ab9bd51125d1fe933282b4efb1a03156b67eb
tanishqv <tanishq7verma@gmail.com>
2022-11-21 17:56:14 +0530

    refactoring the code to use packages

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -3,14 +3,16 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/tanishqv/mywebapp-go/pkg/handlers"
 )
 
 const portNumber = ":8080"
 
 // main is the main application function
 func main() {
-	http.HandleFunc("/", Home)
-	http.HandleFunc("/about", About)
+	http.HandleFunc("/", handlers.Home)
+	http.HandleFunc("/about", handlers.About)
 
 	fmt.Printf("Starting appilcation on %s\n", portNumber)
 	http.ListenAndServe(portNumber, nil)
